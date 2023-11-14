4828c4962c55aadc1710b26aa865326a0b014419
RanggaPrathama <ranggaprathama9@gmail.com>
2023-11-14 22:40:44 +0700

    update load

--- Project_Event/app/Http/Controllers/HomeController.php
+++ Project_Event/app/Http/Controllers/HomeController.php
@@ -19,21 +19,40 @@ class HomeController extends Controller
         return view('pages.user.home');
     }
 
-    public function homepage()
+    public function homepage(Request $request)
     {
-        $events =  DB::select('SELECT
-       MIN(d.harga_event) AS min_harga,
-       e.id_event,
-       e.nama_event,
-       e.gambar_event,
-       DATE_FORMAT(e.tgl_event, "%e %b %y") AS Tanggal_Event,
-       e.nama_lokasi
-   FROM
-       detil_events d
-   JOIN
-       events e ON e.id_event = d.id_event
-   GROUP BY
-       e.id_event, e.nama_event, e.gambar_event, e.tgl_event, e.nama_lokasi');
+        //         $events =  DB::select('SELECT
+        //         MIN(d.harga_event) AS min_harga,
+        //        e.id_event,
+        //        e.nama_event,
+        //        e.gambar_event,
+        //        DATE_FORMAT(e.tgl_event, "%e %b %y") AS Tanggal_Event,
+        //        e.nama_lokasi
+        //         FROM
+        //        detil_events d
+        //    JOIN
+        //        events e ON e.id_event = d.id_event
+        //    GROUP BY
+        //        e.id_event, e.nama_event, e.gambar_event, e.tgl_event, e.nama_lokasi');
+
+        $events = DB::table('detil_events as d')
+            ->select(
+                DB::raw('MIN(d.harga_event) AS min_harga'),
+                'e.id_event',
+                'e.nama_event',
+                'e.gambar_event',
+                DB::raw('DATE_FORMAT(e.tgl_event, "%e %b %y") AS Tanggal_Event'),
+                'e.nama_lokasi'
+            )
+            ->join('events as e', 'e.id_event', '=', 'd.id_event')
+            ->groupBy('e.id_event', 'e.nama_event', 'e.gambar_event', 'e.tgl_event', 'e.nama_lokasi')
+            ->paginate(2);
+
+            if ($request->ajax()) {
+                return response()->json(['html' => view('pages.user.data', compact('events'))->render()]);
+
+            }
+
 
         return view('pages.user.home', ['events' => $events]);
     }
---
+++ Project_Event/resources/views/pages/user/data.blade.php
@@ -0,0 +1,24 @@
+@foreach ($events as $event)
+                    <div class="col-md-4 mb-3">
+                        <div class="card">
+                            <img src="{{ asset('gambarEvent/' . $event->gambar_event) }}" class="card-img-top"
+                                alt="Project 1" />
+                            <div class="card-body text-center">
+                                <h5 class="card-title">{{ $event->nama_event }}</h5>
+                                <p class="card-text">{{ $event->nama_lokasi }}</p>
+                                <a href="{{ route('event') }}" class="btn btn-primary">Pesan Tiket</a>
+                            </div>
+                            <ul class="list-group list-group-flush d-flex justify-content-between">
+                                <li class="list-group-item d-flex justify-content-between">
+                                    <span>Start From</span>
+                                    <p class="text-right">{{ $event->min_harga }}</p>
+                                </li>
+                            </ul>
+                            <div class="date-container">
+                                <span class="date-day">{{ $event->Tanggal_Event }}</span>
+                            </div>
+
+                        </div>
+
+                    </div>
+                @endforeach
